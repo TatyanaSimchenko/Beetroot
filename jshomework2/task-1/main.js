5edfb38203a0f2b70b541016b7ab359ed63450c5
TatyanaSimchenko <misstaty8181@gmail.com>
2020-12-12 11:32:03 +0200

    add

---
+++ jshomework2/task-1/main.js
@@ -0,0 +1,52 @@
+"use strict";
+// Написать функцию, которая принимает 2 числа и возвращает -1,
+//  если первое меньше, чем второе; 
+//  1 – если первое больше, чем второе; 
+//  и 0 – если числа равны.
+// function compare(a, b) {
+//      return a < b ? -1 :
+//       b < a ? 1 : 0;
+//   }
+  // 
+  
+  // for(let i=0; i<10; i++) {
+  //   setTimeout(function() {
+  //     alert(i);
+  //   }, 100);
+  // }
+
+  // for(var i=0; i<10; i++) {
+  //   setTimeout(function() {
+  //     alert(i);
+  //   }, 100);
+  // }
+
+//   let a = 5;
+// alert( a == 5 );
+
+// let arr = [1, 2, 3];
+// arr.something = 5;
+
+// alert(arr.something);
+
+// 
+
+// 
+
+let a = new Array(1,2), b = new Array(3);
+alert(a[0] + b[0]);
+
+// function MyArray() { }
+// MyArray.prototype = [];
+
+// let arr = new MyArray();
+// arr.push(1, 2, 3);
+// alert(arr.length);
+
+  // if (function f(){}) {
+  //   alert(typeof f);
+  // }
+
+//   let obj = {'1': 0, 1: 1, 0: 2};
+
+// alert(obj['1']);
