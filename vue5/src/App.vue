d3ea2222fbc56b8ed29e2d8178bf5394868b0d23
TatyanaSimchenko <misstaty8181@gmail.com>
2021-01-30 17:16:43 +0200

    add vue5

---
+++ vue5/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="post in $store.state.posts" :key="post.id">
+        <h2>{{ post.tatle }}</h2>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
